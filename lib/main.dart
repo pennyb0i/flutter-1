b4c1ee34f5213f498d47c273df3acf47386091ac
Pennyboi <tomas.peniak@gmail.com>
2022-04-21 00:46:50 +0200

    Contact info containers

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 final _random = Random();
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
 }
 
@@ -16,62 +18,69 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  int count = 0;
+  final Future<FirebaseApp> _firebaseApp = Firebase.initializeApp();
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      home: FutureBuilder(
+        future: _firebaseApp,
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return const Text("We made a fucky wucky!");
+          }
+          else if (snapshot.hasData) {
+            return const HomePage();
+          }
+          else {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        },
+      )
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int count = 0;
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
+        backgroundColor: const Color(0xFF0F0F0F),
         appBar: AppBar(
             backgroundColor: Colors.deepPurpleAccent,
             title: const Text('epic gamer moment')
         ),
         body: ListView.builder(itemBuilder: (_,index) {
-          return Container(
-            color: Color.fromRGBO(
-                _random.nextInt(256),
-                _random.nextInt(256),
-                _random.nextInt(256),
-                1.0),
-            width: 50,
-            height: 250,
-            child: Center(
-              child: SizedBox(
-                width: null,
-                height: null,
-                child: ElevatedButton(
-                  child: Text(
-                    '$count + $index',
-                    style: const TextStyle(fontSize: 60),
-                  ),
-                  onPressed: () {
-                    setState(() {
-                      count++;
-                    });
-                  },
-                  onLongPress: () {
-                    setState(() {
-                      count = 0;
-                    });
-                  },
-                ),
-              ),
-            ),
-          );
+          return const ContactEntry();
         }),
         bottomNavigationBar: BottomNavigationBar(
+          backgroundColor: const Color(0xFF0F0F0F),
           items: const [
             BottomNavigationBarItem(
-              icon: Icon(Icons.home),
-              label: 'Home',
+              icon: Icon(Icons.account_box_outlined),
+              activeIcon: Icon(Icons.account_box),
+              label: 'Contacts',
             ),
             BottomNavigationBarItem(
-              icon: Icon(Icons.business),
-              label: 'Business',
+              icon: Icon(Icons.map_outlined),
+              activeIcon: Icon(Icons.map),
+              label: 'Map',
             ),
             BottomNavigationBarItem(
-              icon: Icon(Icons.info),
-              label: 'About',
+              icon: Icon(Icons.message_outlined),
+              activeIcon: Icon(Icons.message),
+              label: 'Messages',
             )
           ],
         ),
@@ -82,4 +91,48 @@ class _MyAppState extends State<MyApp> {
     );
   }
 }
+
+class ContactEntry extends StatelessWidget {
+  const ContactEntry({Key? key}) : super(key: key);
+
+  static const mainTxtStyle = TextStyle(fontSize: 20, color: Colors.blueGrey);
+  static const descTxtStyle = TextStyle(fontSize: 12, color: Colors.grey);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 5),
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.white24, width: 2.0),
+      ),
+      padding: const EdgeInsets.all(15),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(bottom: 10),
+                child: Row(
+                  children: const [
+                    Text("name", style: mainTxtStyle),
+                  ],
+                ),
+              ),
+              Row(
+                children: const [
+                  Text("number ", style: descTxtStyle),
+                  Text("country", style: descTxtStyle),
+                ],
+              ),
+            ],
+          ),
+          const Icon(Icons.account_box, size: 50),
+        ],
+      ),
+    );
+  }
+}
+
+
 
